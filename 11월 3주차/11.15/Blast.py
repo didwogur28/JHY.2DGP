b6446dbef010854e88cc1b6197e5598350fb8624
didwogur28 <didwogur28@naver.com>
2016-11-15 22:39:17 +0900

    11월 15일 수정내용
    11월 15일 수정내용 : 사운드 부분 추가, UI 적용, 충돌 체크

---
+++ 11월 3주차/11.15/Blast.py
@@ -0,0 +1,57 @@
+from pico2d import *
+
+class Blast1:
+    TIME_PER_ACTION = 0.7
+    ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 12
+
+    blastsound = None
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.frame = 0
+        self.total_frame = 0.0
+        self.life_time = 0.0
+        self.image = load_image('object/Missile/Blast.png')
+
+    def update(self, frame_time):
+        self.life_time += frame_time
+        self.total_frame += Blast1.FRAMES_PER_ACTION * Blast1.ACTION_PER_TIME * frame_time
+        self.frame = int(self.total_frame) % 12
+        if (self.frame == 11):
+            return True
+        else:
+            return False
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100,0, 100, 150, self.x, self.y)
+
+class Blast2:
+    TIME_PER_ACTION = 0.7
+    ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 12
+
+    blast2sound = None
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.frame = 0
+        self.total_frame = 0.0
+        self.life_time = 0.0
+        self.image = load_image('object/Missile/Blast2.png')
+
+    def update(self, frame_time):
+        self.life_time += frame_time
+        self.total_frame += Blast2.FRAMES_PER_ACTION * Blast2.ACTION_PER_TIME * frame_time
+        self.frame = int(self.total_frame) % 12
+        if (self.frame == 11):
+            return True
+        else:
+            return False
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 150,0, 150, 200, self.x, self.y)
+
+
